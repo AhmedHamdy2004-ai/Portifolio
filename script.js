5c9bc419bb96881d55490a1378796bdef02a6211
AhmedHamdy2004-ai <ahmedhamdy.ah.2.5.2004@gmail.com>
2025-04-12 13:47:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var typed = new Typed(".multiple-text", {
+  strings: [
+    "Computer Science Student",
+    "Frontend Engineer",
+    "Backend Engineer",
+    "UI/UX Designer",
+  ],
+  typeSpeed: 90,
+  backspeed: 90,
+  backdelay: 1200,
+  loop: true,
+});
+
+var typed = new Typed(".multiple-text-about", {
+  strings: [
+    "Computer Science Student",
+    "Frontend Engineer",
+    "Backend Engineer",
+    "UI/UX Designer",
+  ],
+  typeSpeed: 90,
+  backspeed: 90,
+  backdelay: 1200,
+  loop: true,
+});
